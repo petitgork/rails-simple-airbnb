04960be8e3ff2e89743733f5106f28b5828e52d3
gael carayon <gaelcarayon@hotmail.com>
2023-03-15 17:56:18 +0100

    begin 3

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,33 @@
+class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :update, :delete]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :number_of_guests, :price_per_night)
+  end
+end
